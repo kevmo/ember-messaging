1a9ea012e6276bb121a782077087f70f8a5ce745
kevmo <k.james.moore@gmail.com>
2015-11-12 11:52:57 -0500

    WIP: Saving before I run wild with ember generate.

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 
+// model hook
 export default Ember.Route.extend({
     model() {
-        return ['hi', 'i', 'am']
+        return this.store.findAll('message')
         // return this.store.query('message');
   }
 });
