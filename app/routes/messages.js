506c508ccd5f5d9032e5ef48e3cca6028b37170e
kevmo <k.james.moore@gmail.com>
2015-11-12 11:38:56 -0500

    Route providing assets to hbs templates.

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return this.store.query('message');
+        return ['hi', 'i', 'am']
+        // return this.store.query('message');
   }
 });
