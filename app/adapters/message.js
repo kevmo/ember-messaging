ff2b88736b13c04bc2b25724b5c24d4cee7fcedc
kevmo <k.james.moore@gmail.com>
2015-11-12 13:48:29 -0500

    Properly fetching data; displaying, but needs JSON formatting."

--- app/adapters/message.js
+++ app/adapters/message.js
@@ -1,5 +1,11 @@
-import ApplicationAdapter from './application';
+// import ApplicationAdapter from './application';
+//
+// export default ApplicationAdapter.extend({
+//     host: 'http://private-227b9-jsonapifizzbuzz.apiary-mock.com'
+// });
 
-export default ApplicationAdapter.extend({
+import DS from 'ember-data'
+
+export default DS.JSONAPIAdapter.extend({
     host: 'http://private-227b9-jsonapifizzbuzz.apiary-mock.com'
 });
--- app/routes/messages.js
+++ app/routes/messages.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 // model hook
 export default Ember.Route.extend({
     model() {
-        return this.store.findAll('message')
+        var data = this.store.findAll('message')
+
+        return data
 
         // another way to get the data:
         // var data = Ember.$.getJSON('http://private-227b9-jsonapifizzbuzz.apiary-mock.com/messages');
