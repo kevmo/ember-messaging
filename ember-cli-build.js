7b3c5bacec3f237f7bb5031d1aeede4979fd0736
kevmo <k.james.moore@gmail.com>
2015-11-12 15:04:55 -0500

    Update app build configuration to include Semantic UI CSS.
    It would be a good idea to slim down exactly which CSS assets I include. This will slow down page load and should probably be prioritized as an improvement.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,9 @@ module.exports = function(defaults) {
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
+
+  app.import('bower_components/semantic-ui/dist/semantic.css');
+
   //
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
